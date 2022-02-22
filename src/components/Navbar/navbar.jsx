cf26ac6168999a72fd05030501bb97ad51a27289
jayson7 <lexxiijoo70@gmail.com>
2022-02-22 12:49:57 +0100

    pager

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,13 +1,77 @@
-import React from "react";
-import { FaBars } from "react-icons/fa";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-function Navbar() {
+import { React, useState } from "react";
+import { AiOutlineBars } from "react-icons/ai";
+import Contact from "../Contact/contact";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import HomePage from "../HomePage/home";
+import Order from "../Order/order";
+import Gallery from "../Gallery/gallery";
+import Cart from "../Cart/cart";
+import Category from "../Category/category";
+import "./navbar.css";
+
+export default function NavBar() {
+  const [istoggle, setIsToggle] = useState(false);
+
+  const triggerFunc = () => {
+    if (istoggle === false) {
+      setIsToggle(true);
+    } else {
+      setIsToggle(false);
+    }
+  };
+  console.log(istoggle);
   return (
-    <div>
-      Navbar
-      <FaBars />
-    </div>
+    <Router>
+      <div>
+        <div className="navbar-wrapper">
+          <div className="logo">
+            <Link to="/"> PaGer</Link>
+          </div>
+
+          <div className="toggle" onClick={() => triggerFunc()}>
+            <AiOutlineBars />
+          </div>
+        </div>
+        <div className={istoggle ? "active" : "notactive"}>
+          <ul className=" ">
+            <li>
+              <Link to="/"> Home </Link>
+            </li>
+            <li>
+              <Link to="/order"> Orders</Link>
+            </li>
+            <li>
+              <Link to="/gallery"> Gallery</Link>
+            </li>
+            <li>
+              <Link to="/categories"> Categories</Link>
+            </li>
+            <li>
+              <Link to="/cart"> Cart</Link>
+            </li>
+            <li>
+              <Link to="/contact"> Contact</Link>
+            </li>
+          </ul>
+        </div>
+        {/*
+                          A <Switch> looks through all its children <Route>
+                          elements and renders the first one whose path
+                          matches the current URL. Use a <Switch> any time
+                          you have multiple routes, but you want only one
+                          of them to render at a time
+                          but it has been depreciated in react-router-dom v6
+                        */}
+        <Routes>
+          <Route exact path="/" element={<HomePage />} />
+
+          <Route exact path="/order" element={<Order />} />
+          <Route exact path="/gallery" element={<Gallery />} />
+          <Route exact path="/categories" element={<Category />} />
+          <Route exact path="/cart" element={<Cart />} />
+          <Route exact path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
-
-export default Navbar;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import HomePage from "./components/HomePage/home";
+
 import Navbar from "./components/Navbar/navbar";
 
 ReactDOM.render(
   <React.StrictMode>
-    <HomePage />
     <Navbar />
   </React.StrictMode>,
   document.getElementById("root")
