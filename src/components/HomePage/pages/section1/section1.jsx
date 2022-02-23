6b59f2146d6a2d9d8a448ac029b906c57412f27b
jayson7 <lexxiijoo70@gmail.com>
2022-02-23 11:58:36 +0100

    pager

--- src/components/HomePage/pages/section1/section1.jsx
+++ src/components/HomePage/pages/section1/section1.jsx
@@ -3,7 +3,24 @@ import "./section1.css";
 function Section1() {
   return (
     <div>
-      <div className="section1"></div>
+      <div className="section1">
+        <div className="section1-text-box">
+          <h1>We sell the things you desire, lets get you paged</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur
+            iure id provident nam, nesciunt voluptas accusantium laudantium
+            odio. Eaque, iusto. Lorem, ipsum dolor sit amet consectetur
+            adipisicing elit. Optio quasi aliquid molestiae repudiandae velit in
+            dolor fugit eos doloribus hic non quo quidem soluta voluptatem,
+            libero illo est, explicabo temporibus ipsum. Quia commodi ratione
+            alias? Inventore suscipit commodi eveniet minus.
+          </p>
+          <div className="section1-button">
+            <button>Products</button>
+            <button>Cart</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
