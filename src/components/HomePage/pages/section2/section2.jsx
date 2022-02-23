fa093caa1d7b65fb7865c4c573f7fb45ee6748e6
jayson7 <lexxiijoo70@gmail.com>
2022-02-23 17:45:29 +0100

    pager

--- src/components/HomePage/pages/section2/section2.jsx
+++ src/components/HomePage/pages/section2/section2.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./section2.css";
+import { AiOutlineCheckSquare } from "react-icons/ai";
 
+import img from "./3.jpg";
 function Section2() {
   return (
     <div>
@@ -11,12 +13,19 @@ function Section2() {
         </div>
         <div className="section2-body">
           <div className="info-section2">
-            <div className="division"></div>
+            <div className="division">
+              <div className="check">
+                <AiOutlineCheckSquare />
+              </div>
+              <p> Tested and Trusted </p>
+            </div>
             <div className="division"></div>
             <div className="division"></div>
             <div className="division"></div>
           </div>
-          <div className="img-section2"></div>
+          <div className="img-section2">
+            <img src={img} alt="" />
+          </div>
         </div>
       </div>
     </div>
