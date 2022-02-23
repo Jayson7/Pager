27a04b74a6b9181a9cd76c781677a6f4d5bea5c2
jayson7 <lexxiijoo70@gmail.com>
2022-02-23 14:51:09 +0100

    pager

--- src/components/HomePage/pages/section2/section2.jsx
+++ src/components/HomePage/pages/section2/section2.jsx
@@ -3,7 +3,16 @@ import "./section2.css";
 function Section2() {
   return (
     <div>
-      <div className="section2"></div>
+      <div className="section2">
+        <div className="section2-title">
+          <h6>Integrity</h6>
+          <h1>Why Shop With US</h1>
+        </div>
+        <div className="section2-body">
+          <div className="info-section2"></div>
+          <div className="img-section2"></div>
+        </div>
+      </div>
     </div>
   );
 }
