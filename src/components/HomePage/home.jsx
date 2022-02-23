99272eed178ead33119fdf8f72c75628f5dcbd64
jayson7 <lexxiijoo70@gmail.com>
2022-02-23 14:37:47 +0100

    pager

--- src/components/HomePage/home.jsx
+++ src/components/HomePage/home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Section1 from "./pages/section1/section1";
+import Section2 from "./pages/section2/section2";
 function HomePage() {
   return (
     <div>
       <Section1 />
+      <Section2 />
     </div>
   );
 }
---
+++ src/components/HomePage/pages/section2/section2.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./section2.css";
+function Section2() {
+  return (
+    <div>
+      <div className="section2"></div>
+    </div>
+  );
+}
+
+export default Section2;
