d45cd2b18314de6abca915b036ce76be3cba9aa8
jayson7 <lexxiijoo70@gmail.com>
2022-02-24 16:06:12 +0100

    pager

--- src/components/HomePage/home.jsx
+++ src/components/HomePage/home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Section1 from "./pages/section1/section1";
 import Section2 from "./pages/section2/section2";
 import Section3 from "./pages/section3/section3";
+import Section4 from "./pages/section4/section4";
 function HomePage() {
   return (
     <div>
       <Section1 />
       <Section2 />
       <Section3 />
+      <Section4 />
     </div>
   );
 }
---
+++ src/components/HomePage/pages/section4/section4.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./section4.css";
+function Section4() {
+  return (
+    <div>
+      <div className="section4">
+        <div className="section4-title">
+          <h4>Offers</h4>
+          <h1>What we offer</h1>
+        </div>
+        <div className="section4-wrapper">
+          <div className="item-section4"></div>
+          <div className="item-section4"></div>
+          <div className="item-section4"></div>
+          <div className="item-section4"></div>
+          <div className="item-section4"></div>
+          <div className="item-section4"></div>
+          <div className="item-section4"></div>
+          <div className="item-section4"></div>
+          <div className="item-section4"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Section4;
