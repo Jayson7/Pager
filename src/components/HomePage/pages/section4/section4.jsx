6cd61b8a37fdcd9e768a6d8a83f6477ee2190533
jayson7 <lexxiijoo70@gmail.com>
2022-02-24 16:16:57 +0100

    pager

--- src/components/HomePage/pages/section4/section4.jsx
+++ src/components/HomePage/pages/section4/section4.jsx
@@ -5,8 +5,8 @@ function Section4() {
     <div>
       <div className="section4">
         <div className="section4-title">
-          <h4>Offers</h4>
-          <h1>What we offer</h1>
+          <h4>in store</h4>
+          <h1> Lastest products</h1>
         </div>
         <div className="section4-wrapper">
           <div className="item-section4"></div>
