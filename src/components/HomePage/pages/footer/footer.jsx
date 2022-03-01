cdfb011475e0af686027b0d8fca985110ea1bc7f
jayson7 <lexxiijoo70@gmail.com>
2022-03-01 16:52:57 +0100

    grid

--- src/components/HomePage/pages/footer/footer.jsx
+++ src/components/HomePage/pages/footer/footer.jsx
@@ -3,7 +3,13 @@ import "./footer.css";
 function Footer() {
   return (
     <div>
-      <div className="footer">Footer</div>
+      <div className="footer">
+        <div className="footer-wrapper">
+          <div className="foot1"></div>
+          <div className="foot2"></div>
+        </div>
+        <div className="copy">&copy; Jayson</div>
+      </div>
     </div>
   );
 }
