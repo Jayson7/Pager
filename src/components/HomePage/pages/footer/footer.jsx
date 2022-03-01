fe393b32bdf1a558689522a6aedfd260282480d4
jayson7 <lexxiijoo70@gmail.com>
2022-03-01 19:38:50 +0100

    grid

--- src/components/HomePage/pages/footer/footer.jsx
+++ src/components/HomePage/pages/footer/footer.jsx
@@ -5,8 +5,26 @@ function Footer() {
     <div>
       <div className="footer">
         <div className="footer-wrapper">
-          <div className="foot1"></div>
-          <div className="foot2"></div>
+          <div className="foot1">
+            <h4>Do you love to code ?</h4>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
+              quis quibusdam sed nulla maiores non ipsum vero, rerum sequi
+              officia ratione nesciunt cupiditate dolor dolorem expedita totam
+              quas inventore tempore veniam quidem a adipisci. Sapiente harum
+              nesciunt aliquam rem?
+            </p>
+          </div>
+          <div className="foot2">
+            <ul>
+              <li>Kitchen Materials</li>
+              <li>Phone Accesories</li>
+              <li>Laptops</li>
+              <li>Clothes</li>
+              <li>Shoes</li>
+              <li>Wrist watches</li>
+            </ul>
+          </div>
         </div>
         <div className="copy">&copy; Jayson</div>
       </div>
